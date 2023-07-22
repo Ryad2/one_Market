395326e3d78b1834134ee737838a6afdb3a3ab1b
Noé Zaabi <noe@Noes-MacBook-Pro.local>
2023-07-22 17:34:38 +0200

    account page

---
+++ src/components/ui/card.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import styles from "./Card.module.css";
+
+interface CardProps {
+  title: string;
+}
+
+const Card: React.FC<CardProps> = ({ title }: CardProps) => {
+  return (
+    <div className={styles.mainCard}>
+      <img className={styles.cardImg} src="/froyo_pauvre.png" alt="" />
+      <div className={styles.cardTitle}>{title ? title : ""}</div>
+      <div className={styles.cardPrice}>
+        <div>122 $USDC</div>
+      </div>
+    </div>
+  );
+};
+Card.displayName = "Card";
+
+export default Card;
---
+++ src/pages/marketplace/index.tsx
@@ -0,0 +1,57 @@
+import Link from "next/link";
+
+import Header from "../../components/ui/header";
+import Card from "../../components/ui/card";
+
+import styles from "./Account.module.css";
+import { useSession } from "next-auth/react";
+
+export default function Account() {
+  const session = useSession();
+  return (
+    <div className={styles.containerAccount}>
+      <Header title="" />
+      <div className={styles.mainAccount}>
+        <div className={styles.leftAccount}>
+          <div className={styles.divImgAccount}>
+            <div>
+              <img src="/nouns.png" alt="" className={styles.imgAccount} />
+            </div>
+            <div className={styles.nameAccount}>{session.data?.user.name}</div>
+          </div>
+          <div className={styles.msgAccount}>Messages</div>
+          <div className={styles.settingAccount}>Setting</div>
+          <div className={styles.sellAccount}>Sell a product</div>
+        </div>
+        <div className={styles.rightAccount}>
+          <div>
+            <div className={styles.nameCategAccount}>Last products boughts</div>
+            <div className={styles.carouselAccount}>
+              <div>
+                <Link href="/product/4RREH34JZ">
+                  <Card title="Very very cool bike" />
+                </Link>
+                <Card title="Very very cool bike" />
+                <Card title="Very very cool bike" />
+                <Card title="Very very cool bike" />
+                <Card title="Very very cool bike" />
+                <Card title="Very very cool bike" />
+              </div>
+            </div>
+            <div className={styles.nameCategAccount}>Last products seen</div>
+            <div className={styles.carouselAccount}>
+              <div>
+                <Card title="Very very cool bike" />
+                <Card title="Very very cool bike" />
+                <Card title="Very very cool bike" />
+                <Card title="Very very cool bike" />
+                <Card title="Very very cool bike" />
+                <Card title="Very very cool bike" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
